0c7f7e2e081f2ef6282ffb99ed729fcd08e1ca82
davidcoats <david@getsafetycone.com>
2022-06-09 12:21:27 -0700

    Interim changes.

--- source/R5T.D0081.I001/Code/Bases/Extensions/IServiceActionExtensions.cs
+++ source/R5T.D0081.I001/Code/Bases/Extensions/IServiceActionExtensions.cs
@@ -11,7 +11,7 @@ namespace R5T.D0081.I001
     public static class IServiceActionExtensions
     {
         /// <summary>
-        /// Adds the <see cref="ConstructorBasedExecutionSynchronicityProvider"/> implementation of <see cref="IExecutionSynchronicityProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// Adds the <see cref="ConstructorBasedExecutionSynchronicityProvider"/> implementation of <see cref="IExecutionSynchronicityProvider"/> as a <see cref="Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton"/>.
         /// </summary>
         public static IServiceAction<IExecutionSynchronicityProvider> AddConstructorBasedExecutionSynchronicityProviderAction(this IServiceAction _,
             Synchronicity synchronicity)
@@ -23,7 +23,7 @@ namespace R5T.D0081.I001
         }
 
         /// <summary>
-        /// Adds the <see cref="HardCodedSynchronousExecutionSynchronicityProvider"/> implementation of <see cref="IExecutionSynchronicityProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// Adds the <see cref="HardCodedSynchronousExecutionSynchronicityProvider"/> implementation of <see cref="IExecutionSynchronicityProvider"/> as a <see cref="Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton"/>.
         /// </summary>
         public static IServiceAction<IExecutionSynchronicityProvider> AddHardCodedSynchronousExecutionSynchronicityProviderAction(this IServiceAction _)
         {
@@ -32,7 +32,7 @@ namespace R5T.D0081.I001
         }
 
         /// <summary>
-        /// Adds the <see cref="HardCodedAsynchronousExecutionSynchronicityProvider"/> implementation of <see cref="IExecutionSynchronicityProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// Adds the <see cref="HardCodedAsynchronousExecutionSynchronicityProvider"/> implementation of <see cref="IExecutionSynchronicityProvider"/> as a <see cref="Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton"/>.
         /// </summary>
         public static IServiceAction<IExecutionSynchronicityProvider> AddHardCodedAsynchronousExecutionSynchronicityProviderAction(this IServiceAction _)
         {
